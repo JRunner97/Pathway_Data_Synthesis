e45afa067c3c25d02a3ce0550d7a249aed3745af
JRunner97 <33672983+JRunner97@users.noreply.github.com>
2021-02-22 17:30:11 -0600

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -2,6 +2,9 @@ import json
 import os
 import numpy as np
 import cv2
+import scipy.fftpack as fp
+from scipy import stats
+from matplotlib import pyplot as plt
 
 relation_slices = []
 
@@ -81,7 +84,9 @@ for relation in relations:
     relation_elements.append([element1_json,element2_json])
 
 
+# remove backgrounds and artifacts from relation slices
 processed_slices = []
+masks = []
 for img in relation_slices:
 
     # greyscale and filter threshold
@@ -100,4 +105,91 @@ for img in relation_slices:
     invert_mask = np.invert(mask)
     new_img = cv2.bitwise_or(img, invert_mask)
 
+    masks.append(mask)
     processed_slices.append(new_img)
+
+
+# read template and get query coords
+template_im = cv2.imread('templates/template3.png')
+
+
+def radial_profile(data, center):
+    y, x = np.indices((data.shape))
+    r = np.sqrt((x - center[0])**2 + (y - center[1])**2)
+    r = r.astype(np.int)
+
+    # sum over pixels the same radius away from center
+    tbin = np.bincount(r.ravel(), data.ravel())
+
+    # normalize by distance to center, since as radius grows the # of pixels in radius bin does also
+    nr = np.bincount(r.ravel())
+    radialprofile = tbin / nr
+    return radialprofile 
+
+def check_slice(template_im,slice_shape,x,y):
+
+    template_slice = template_im[y_target:y_target+slice_shape[0],x_target:x_target+slice_shape[1],:]
+    grey_slice = cv2.cvtColor(template_slice, cv2.COLOR_BGR2GRAY)
+    f = np.fft.fft2(grey_slice)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+    # x,y format
+    center = (int(slice_shape[1] / 2),int(slice_shape[0] / 2))
+
+    # get description of fft emitting from center
+    radial_prof = radial_profile(magnitude_spectrum, center)
+
+    idx = range(0,radial_prof.shape[0])
+    bin_means = stats.binned_statistic(idx, radial_prof, 'mean', bins=4)[0]
+    
+    if bin_means[-1] < 50 and bin_means[-2] < 50:
+        return True
+    else:
+        return False
+
+
+current_slice = processed_slices[0]
+current_mask = masks[0]
+
+# check if queried coords are a valid location
+for idx in range(50):
+
+    # subtracted max bounds to ensure valid coords
+    slice_shape = current_slice.shape
+    x_target = np.random.randint(0,template_im.shape[1]-slice_shape[1])
+    y_target = np.random.randint(0,template_im.shape[0]-slice_shape[0])
+    
+    # check if selected template area is good
+    if check_slice(template_im,slice_shape,x_target,y_target):
+
+        template_slice = template_im[y_target:y_target+slice_shape[0],x_target:x_target+slice_shape[1],:]
+
+        # add slice
+        template_slice = cv2.bitwise_xor(template_slice, current_mask)
+        template_im[y_target:y_target+slice_shape[0],x_target:x_target+slice_shape[1],:] = template_slice
+
+   
+
+
+
+cv2.imwrite('dst_rt.png', template_im)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+
+
+
+# template_im2 = cv2.imread('templates/template4.png',0)
+# # template_im2 = cv2.cvtColor(template_im, cv2.COLOR_BGR2GRAY)
+# f = np.fft.fft2(template_im2)
+# fshift = np.fft.fftshift(f)
+# magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+# plt.subplot(121),plt.imshow(template_im2, cmap = 'gray')
+# plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+# plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+# plt.show()
