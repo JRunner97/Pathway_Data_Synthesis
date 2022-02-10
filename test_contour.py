c785a38eb19cd616cba4233ef07a5cf8d2147a8a
JRunner97 <33672983+JRunner97@users.noreply.github.com>
2022-02-10 15:24:38 -0600

    finished v1 gen shape

--- test_contour.py
+++ test_contour.py
@@ -3,29 +3,6 @@ from matplotlib.pyplot import axis
 import numpy as np
 import random
 
-# image = cv2.imread('2.jpg')
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('image', gray)
-# cv2.waitKey()
-# thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)[1]
-# cv2.imshow('image', thresh)
-# cv2.waitKey()
-# cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-# cnts = cnts[0][1]
-
-# img = np.ones((225,225,3), np.uint8) * 255
-
-# dist = cv2.pointPolygonTest(cnts,(5,5),True)
-# img = cv2.circle(img, (5,5), 2, (0,0,0), 1)
-# print(dist)
-
-# cnts = cnts.reshape((-1,2))
-# for cnt in cnts:
-#     img = cv2.circle(img, (cnt), 2, (0,0,0), 1)
-
-#     cv2.imshow('image', img)
-#     cv2.waitKey()
-
 class Synthetic_Rectangle:
     def __init__(self, center, label, im_path):
         self.center = center
@@ -46,11 +23,17 @@ class Synthetic_Rectangle:
 
     def draw_shape(self,img,center):
 
+        cnts = self.cnt.reshape((-1,2))
+        for cnt in cnts:
+            img = cv2.circle(img, (cnt), 2, (0,0,0), -1)
         
         return img
 
     def get_points(self):
 
+        cnt = self.cnt.reshape((-1,2))
+        cols = cnt[:,0]
+        rows = cnt[:,1]
 
         return rows, cols
 
@@ -73,6 +56,3 @@ class Synthetic_Rectangle:
 
         return mins[0], maxes[0], mins[1], maxes[1]
 
-my_shape = Synthetic_Rectangle([0,0],'cats','2.jpg')
-tmp_min_x, tmp_max_x, tmp_min_y, tmp_max_y = my_shape.get_min_max()
-print(tmp_max_x)
